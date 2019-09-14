aa50ffe6239578ecacbf23099e95cd7c82aecc0f
Tom Hanks <tommy.hanks@gmail.com>
2019-09-14 06:52:36 -0400

    Added in AutoFac along with a janky service locator thing.

---
+++ src/GuiR/Configuration/ServiceLocator.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+
+namespace GuiR.Configuration
+{
+    public static class ServiceLocator
+    {
+        private static readonly IContainer _container;
+        private static readonly ILifetimeScope _scope;
+
+        static ServiceLocator()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyModules(typeof(App).Assembly);
+
+            _container = builder.Build();
+
+            _scope = _container.BeginLifetimeScope();
+        }
+
+        public static TService GetService<TService>() =>
+            _scope.Resolve<TService>();
+
+        public static void Dispose()
+        {
+            _scope.Dispose();
+            _container.Dispose();
+        }
+    }
+}
