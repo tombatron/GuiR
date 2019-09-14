7fd727c380c6123640c294ce3e464140bb4155f4
Tom Hanks <tommy.hanks@gmail.com>
2019-09-14 06:53:56 -0400

    Cleaned up here a little bit.
    Added in the ServiceLocator clean up.

--- src/GuiR/App.xaml.cs
+++ src/GuiR/App.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GuiR.Configuration;
 using System.Windows;
 
 namespace GuiR
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            ServiceLocator.Dispose();
+        }
     }
 }
