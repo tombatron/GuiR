d9c935d1ad6c3236295f3f9debe6270c3e309f4b
Tom Hanks <tommy.hanks@gmail.com>
2019-09-21 05:47:01 -0400

    Felt cute, thought I would lay down an interface for actually saving those settings that you put in when adding a server.

---
+++ src/GuiR/Settings/ISettingsProvider.cs
@@ -0,0 +1,13 @@
+﻿using GuiR.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GuiR.Settings
+{
+    public interface ISettingsProvider
+    {
+        Task<IEnumerable<RedisServerInformation>> GetServerSettingsAsync();
+
+        Task SaveServerSettingsAsync(IEnumerable<RedisServerInformation> serverSettings);
+    }
+}
