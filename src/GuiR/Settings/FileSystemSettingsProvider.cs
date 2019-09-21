e72c3add2576bdac2d7e94365e6f2600448a563e
Tom Hanks <tommy.hanks@gmail.com>
2019-09-21 13:57:57 -0400

    Initial implementation of the file system settings provider done.

--- src/GuiR/Settings/FileSystemSettingsProvider.cs
+++ src/GuiR/Settings/FileSystemSettingsProvider.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("GuiR.Tests")]
 
 namespace GuiR.Settings
 {
@@ -11,15 +14,26 @@ namespace GuiR.Settings
         public FileSystemSettingsProvider()
         {
             var localApplicationDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            
+
             FileLocation = Path.Combine(localApplicationDataPath, "GuiR", "servers.config");
         }
 
         public string FileLocation { get; }
 
-        public Task<IEnumerable<RedisServerInformation>> GetServerSettingsAsync()
+        public async Task<IEnumerable<RedisServerInformation>> GetServerSettingsAsync()
         {
-            throw new NotImplementedException();
+            var result = new List<RedisServerInformation>();
+
+            using (var file = File.OpenRead(FileLocation))
+            using (var reader = new StreamReader(file))
+            {
+                var configurationLine = await reader.ReadLineAsync();
+                var parsedConfiguration = ParseConfigurationLine(configurationLine);
+
+                result.Add(parsedConfiguration);
+            }
+
+            return result;
         }
 
         public async Task SaveServerSettingsAsync(IEnumerable<RedisServerInformation> serverSettings)
@@ -27,11 +41,34 @@ namespace GuiR.Settings
             using (var file = File.Create(FileLocation))
             using (var writer = new StreamWriter(file))
             {
-                foreach(var serverSetting in serverSettings)
+                foreach (var serverSetting in serverSettings)
                 {
-                    await writer.WriteAsync($"{serverSetting.ServerName}|{serverSetting.ServerAddress}|{serverSetting.ServerPort}\n");
+                    await writer.WriteAsync($"{serverSetting.ServerName.Trim()}|{serverSetting.ServerAddress.Trim()}|{serverSetting.ServerPort}\n");
                 }
             }
+        }
+
+        internal static RedisServerInformation ParseConfigurationLine(string configurationLine)
+        {
+            string serverName;
+            string serverAddress;
+            string serverPort;
+
+            var configurationLineSpan = configurationLine.AsSpan();
+
+            var firstDelimiter = configurationLineSpan.IndexOf('|');
+            var secondDelimiter = configurationLineSpan.LastIndexOf('|');
+
+            serverName = configurationLineSpan.Slice(0, firstDelimiter).ToString();
+            serverAddress = configurationLineSpan.Slice(serverName.Length + 1, (secondDelimiter - serverName.Length) - 1).ToString();
+            serverPort = configurationLineSpan.Slice(secondDelimiter + 1, (configurationLine.Length - secondDelimiter) - 2).ToString();
+
+            return new RedisServerInformation
+            {
+                ServerName = serverName,
+                ServerAddress = serverAddress,
+                ServerPort = int.Parse(serverPort)
+            };
         }
     }
 }
---
+++ test/GuiR.Tests/Settings/FileSystemSettingsProviderTests.cs
@@ -0,0 +1,23 @@
+﻿using GuiR.Settings;
+using Xunit;
+
+namespace GuiR.Tests.Settings
+{
+    public class FileSystemSettingsProviderTests
+    {
+        public class ParseConfigurationLineCan
+        {
+            [Fact]
+            public void ParseConfigurationLine()
+            {
+                var exampleConfigurationLine = "name|host|1234\n";
+
+                var parsedConfigurationLine = FileSystemSettingsProvider.ParseConfigurationLine(exampleConfigurationLine);
+
+                Assert.Equal("name", parsedConfigurationLine.ServerName);
+                Assert.Equal("host", parsedConfigurationLine.ServerAddress);
+                Assert.Equal(1234, parsedConfigurationLine.ServerPort);
+            }
+        }
+    }
+}
