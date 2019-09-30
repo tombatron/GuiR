ff66362862fb65c5d83ad18a4df73f882f0a3466
Tom Hanks <tommy.hanks@gmail.com>
2019-09-30 05:41:28 -0400

    Added in a new view model for the hash display.

---
+++ src/GuiR/ViewModels/Keys/KeyDisplay/HashKeyViewModel.cs
@@ -0,0 +1,40 @@
+﻿using GuiR.Models;
+using GuiR.Redis;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace GuiR.ViewModels.Keys.KeyDisplay
+{
+    public class HashKeyViewModel : ViewModelBase
+    {
+        private readonly RedisProxy _redis;
+
+        public HashKeyViewModel(RedisProxy redis) => _redis = redis;
+
+        public string Key { get; set; }
+
+        public IEnumerable<HashCollectionEntry> _keyValue;
+        public IEnumerable<HashCollectionEntry> KeyValue
+        {
+            get => _keyValue;
+
+            set
+            {
+                _keyValue = value;
+
+                RaisePropertyChangedEvent(nameof(KeyValue));
+            }
+        }
+
+        public RedisServerInformation ServerInfo { get; set; }
+
+        public int DatabaseId { get; set; }
+
+        public ICommand LoadKeyValue =>
+            new DelegateCommand(async () => 
+            {
+                KeyValue = await _redis.GetHashAsync(ServerInfo, DatabaseId, Key);
+            });
+
+    }
+}
