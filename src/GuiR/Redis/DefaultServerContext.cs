3ac50fcdc3daa26b7dea57cd8badaac0ec660d74
Tom Hanks <tommy.hanks@gmail.com>
2019-10-02 05:28:14 -0400

    Since I think it might make some stuff easier to handle, I'm going to maintain a "global" context of what server / database / key is currently selected instead of continually passing that information around.
    I'm introducing this interface/class so that I have a prayer of keeping
    this stuff loosely coupled.

---
+++ src/GuiR/Redis/DefaultServerContext.cs
@@ -0,0 +1,11 @@
+﻿using GuiR.Models;
+
+namespace GuiR.Redis
+{
+    class DefaultServerContext : IServerContext
+    {
+        public RedisServerInformation ServerInfo { get; set; }
+        public int DatabaseId { get; set; }
+        public string SelectedKey { get; set; }
+    }
+}
---
+++ src/GuiR/Redis/IServerContext.cs
@@ -0,0 +1,13 @@
+﻿using GuiR.Models;
+
+namespace GuiR.Redis
+{
+    interface IServerContext
+    {
+        RedisServerInformation ServerInfo { get; set; }
+
+        int DatabaseId { get; set; }
+
+        string SelectedKey { get; set; }
+    }
+}
