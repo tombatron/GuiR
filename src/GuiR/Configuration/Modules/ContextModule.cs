1fd0c5dd17a9491f2fac96a9883f8f19d4e281d1
Tom Hanks <tommy.hanks@gmail.com>
2019-10-02 05:38:04 -0400

    Registered that new server context idea with ioc.

---
+++ src/GuiR/Configuration/Modules/ContextModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using GuiR.Redis;
+
+namespace GuiR.Configuration.Modules
+{
+    class ContextModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<DefaultServerContext>().As<IServerContext>().SingleInstance();
+        }
+    }
+}
