ac2c16cf74f16e67059c658d0773f84c96876b33
Tom Hanks <tommy.hanks@gmail.com>
2019-10-03 07:00:54 -0400

    Added in control for view sorted sets.

---
+++ src/GuiR/Controls/KeyDisplay/SortedSetKey.xaml.cs
@@ -0,0 +1,19 @@
+﻿using GuiR.Configuration;
+using GuiR.ViewModels.Keys.KeyDisplay;
+using System.Windows.Controls;
+
+namespace GuiR.Controls.KeyDisplay
+{
+    public partial class SortedSetKey : UserControl
+    {
+        public SortedSetKey(string key)
+        {
+            var viewModel = ServiceLocator.GetService<SortedSetKeyViewModel>();
+            viewModel.Key = key;
+
+            DataContext = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
