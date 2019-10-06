7fbce91b5d6db246b6a47695136dfda78b52cf31
Tom Hanks <tommy.hanks@gmail.com>
2019-10-06 08:19:28 -0400

    Add this here even though hyperloglog isn't a distinct data type...

--- src/GuiR/Redis/RedisTypes.cs
+++ src/GuiR/Redis/RedisTypes.cs
@@ -7,5 +7,6 @@
         public const string HashType = "hash";
         public const string SetType = "set";
         public const string SortedSetType = "sortedset";
+        public const string HyperLogLog = "hyperloglog";
     }
 }
