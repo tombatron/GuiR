874b31295baf9d4744458d84767544edae41632b
Tom Hanks <tommy.hanks@gmail.com>
2019-10-19 06:41:11 -0400

    This "powers" the virtualizing list.

---
+++ src/GuiR/Models/KeyItemsProvider.cs
@@ -0,0 +1,25 @@
+﻿using GuiR.Models.Virtualization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GuiR.Models
+{
+    public class KeyItemsProvider : IItemsProvider<string>
+    {
+        private IEnumerable<string> _keyEnumerable;
+
+        public KeyItemsProvider(IEnumerable<string> keyEnumerable) => _keyEnumerable = keyEnumerable;
+
+        public int FetchCount()
+        {
+            return 1_000_000;
+        }
+
+        public IList<string> FetchRange(int startIndex, int count)
+        {
+            _keyEnumerable = _keyEnumerable.Skip(startIndex);
+
+            return _keyEnumerable.Take(count).ToList();
+        }
+    }
+}
